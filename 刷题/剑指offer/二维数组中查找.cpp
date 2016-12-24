a3e33541df7f89602cb3f8fe27cdc80542fa4380
Johnxs <849886839@qq.com>
2016-12-24 21:06:26 +0800

    二维数组的查找

---
+++ 刷题/剑指offer/二维数组中查找.cpp
@@ -0,0 +1,29 @@
+//using namespace std;
+// 
+// int arr[4][4] = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+// 
+// bool find(const int num)
+// {
+//    int i = 0;
+//    int j = 3;
+//
+//   while(i <= 3 && j >= 0 )
+//    {
+//        if(num < arr[i][j])
+//           --j;
+//        else if(num > arr[i][j])
+//            ++i;
+//         else
+//             return true;
+//     }
+//     return false;
+// }                                                                                                                                          
+// 
+// int main()
+// {
+//     int num = 0;
+//     cin>>num;
+// 
+//     cout<<find(num)<<endl;
+//     return 0;
+// }
